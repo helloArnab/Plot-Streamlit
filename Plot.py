8e3523f9d5b9a4d939342d5032c4dc2c935a4cce
Arnab Mandal <99715406+helloArnab@users.noreply.github.com>
2023-11-01 09:31:34 +0530

    Update Plot.py

--- Plot.py
+++ Plot.py
@@ -13,7 +13,7 @@ st.altair_chart(chart)
 
 st.header('2. Interactive Charts')
 st.subheader('2.1 Line Chart')
-df = pd.read_csv("D:\Data Science\Streamlit\lang_data.csv")
+df = pd.read_csv("lang_data.csv")
 lang_list = df.columns.tolist()
 lang_choices = st.multiselect('Choose your language : ',lang_list)
 new_df = df[lang_choices]
@@ -24,7 +24,7 @@ st.area_chart(new_df)
 
 st.header('3. Data Visualisation with Plotly')
 st.subheader('3.1 Displaying the dataset')
-df = pd.read_csv("D:\Data Science\Streamlit\\tips.csv")
+df = pd.read_csv("tips.csv")
 st.dataframe(df.head())
 
 st.subheader('3.2 Pie Chart')
